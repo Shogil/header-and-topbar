04bbf3e6c53c8a5e6de72eed493b5383ca8fc81a
shogil <shogilbabu@gmail.com>
2021-09-19 18:27:50 +0530

    update link

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './NavBar.css'
 import Mainlogo from './'
+import {Link} from 'react-router-dom'
 
 function NavBar() {
     return (
         
             <div className="navbar">
-              <img className="logo" src={} alt="NavBar" />
+              <img className="logo" src={Mainlogo} alt="NavBar" />
               <div className="navbar-right">
                   <Link to="/" className="right">Home</Link>
                   <Link to="/about" className="right">About</Link>
