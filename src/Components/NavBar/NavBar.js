40b7f98a6676712500b355e9fef4fa209963fc66
shogil <shogilbabu@gmail.com>
2021-09-19 14:12:07 +0530

    navbar css added

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import './NavBar.css'
+import Mainlogo from './'
 
 function NavBar() {
     return (
-        <div>
+        
+            <div className="navbar">
+              <img className="logo" src={} alt="NavBar" />
+              <div className="navbar-right">
+                  <Link to="/" className="right">Home</Link>
+                  <Link to="/about" className="right">About</Link>
+                  <Link to="/promote" className="right">Promote</Link>
+
+              </div>
+            </div>
             
-        </div>
+        
     )
 }
 
